aba8ecfa1bc45d6b5827fb40df31d031d3598b6a
MassiJimmy <92297534+MassiJimmy@users.noreply.github.com>
2023-12-03 13:51:41 +0100

    Initial commit

---
+++ FindTheDuplicate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FindTheDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ListNb = new int[8];
+            var rand = new Random();
+            int Theduplicate = 0;
+            
+            for(int i=0; i<8;i++)
+            {
+                ListNb[i] = rand.Next(1, 10);
+                Console.WriteLine(ListNb[i]);
+            }
+            Console.WriteLine(" ");
+
+                for (int j = 0; j < ListNb.Length; j++)
+                {
+                    for (int i = 0; i < ListNb.Length; i++)
+                    {
+                        if (ListNb[i] == ListNb[j] && i != j)
+                        {
+                            Theduplicate = ListNb[i];
+
+                            Console.WriteLine(Theduplicate);
+                    }
+                    }
+                }
+            Console.WriteLine(Theduplicate);
+            Console.ReadKey();
+        }
+    }
+}
